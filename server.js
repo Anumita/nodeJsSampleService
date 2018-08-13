8d665f04059b4c0782f004a85a996af2deefdd85
ansheno <anumitashenoy@gmail.com>
2018-08-13 19:02:26 +0530

    U4

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Hello Docker hub -update 3. CDshould automatically update the azure web app.\n');
+  res.send('Hello Docker hub -update 4. CDshould automatically update the azure web app.\n');
 });
 
 app.listen(PORT, HOST);
